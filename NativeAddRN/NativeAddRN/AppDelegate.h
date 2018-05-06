473d35467be9fd130a85b7f52a4f646edc7ff68b
罗章 <luozhang002@126.com>
2018-05-06 21:43:50 +0800

    feat：添加一个可以跑起来的版本

---
+++ NativeAddRN/NativeAddRN/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NativeAddRN
+//
+//  Created by 罗章 on 2018/5/6.
+//  Copyright © 2018年 罗章. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
